21e572e98297e79ad833024116b937cf9d0311d2
Alexander W. Buckstiegel <awbuckst@csis.svsu.edu>
2020-02-22 20:15:00 -0500

    updated index.php

--- crud/index.php
+++ crud/index.php
@@ -7,47 +7,13 @@
 </head>
 
 <body>
-    <div class="container">
-    		<div class="row">
-    			<h3>Customer File</h3>
-    		</div>
-			<div class="row">
-				<p>
-					<a href="create.php" class="btn btn-success">Create</a>
-				</p>
-				
-				<table class="table table-striped table-bordered">
-		              <thead>
-		                <tr>
-		                  <th>Name</th>
-		                  <th>Email Address</th>
-		                  <th>Mobile Number</th>
-		                  <th>Action</th>
-		                </tr>
-		              </thead>
-		              <tbody>
-		              <?php 
-					   require 'database.php';
-					   $pdo = Database::connect();
-					   $sql = 'SELECT * FROM customers ORDER BY id DESC';
-	 				   foreach ($pdo->query($sql) as $row) {
-						   		echo '<tr>';
-							   	echo '<td>'. $row['name'] . '</td>';
-							   	echo '<td>'. $row['email'] . '</td>';
-							   	echo '<td>'. $row['mobile'] . '</td>';
-							   	echo '<td width=250>';
-							   	echo '<a class="btn" href="read.php?id='.$row['id'].'">Read</a>';
-							   	echo '&nbsp;';
-							   	echo '<a class="btn btn-success" href="update.php?id='.$row['id'].'">Update</a>';
-							   	echo '&nbsp;';
-							   	echo '<a class="btn btn-danger" href="delete.php?id='.$row['id'].'">Delete</a>';
-							   	echo '</td>';
-							   	echo '</tr>';
-					   }
-					   Database::disconnect();
-					  ?>
-				      </tbody>
-	            </table>
+    <div>
+		<p>
+			<a href = "customers.php">customers.php</a><br>			
+			<a href = "customers.php">events.php</a><br>
+			<a href = "https://github.com/awbuckst11181999/CIS355_CRUD1">Github for As03</a><br>
+			
+
     	</div>
     </div> <!-- /container -->
   </body>
